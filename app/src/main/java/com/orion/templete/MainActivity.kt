269c8052fb784c176294b2d1b463dee43d69eadf
utkarsh singh <78771861+utkarshxf@users.noreply.github.com>
2024-04-20 01:30:00 +0530

    .

--- app/src/main/java/com/orion/templete/MainActivity.kt
+++ app/src/main/java/com/orion/templete/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.orion.templete
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,7 +13,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.orion.templete.domain.use_case.Screen
 import com.orion.templete.presentation.MainScreen
-import com.orion.templete.presentation.OnboardingScreen
 import com.orion.templete.presentation.ui.theme.TempleteTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,37 +22,22 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
-
             TempleteTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    val route = if (onBoardingIsFinished(this@MainActivity)) {
-                        Screen.MainScreen.route
-                    } else {
-                        Screen.OnboardingScreen.route
-                    }
                     NavHost(
                         navController = navController,
-                        startDestination = route
+                        startDestination = Screen.MainScreen.route
                     ) {
                         composable(route = Screen.MainScreen.route) {
                             MainScreen()
                         }
-                        composable(route = Screen.OnboardingScreen.route) {
-                            OnboardingScreen(navController , context = this@MainActivity)
-                        }
-
                     }
                 }
             }
         }
     }
-}
-private fun onBoardingIsFinished(context: MainActivity): Boolean {
-    val sharedPreferences = context.getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
-    return sharedPreferences.getBoolean("isFinished", false)
-
 }
