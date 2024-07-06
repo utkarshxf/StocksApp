56098449407b9972c8e58bee9904e38d46805d78
utkarsh singh <78771861+utkarshxf@users.noreply.github.com>
2024-07-06 23:47:26 +0530

    feat : Update

--- app/src/main/java/com/orion/templete/ui/components/CostomBar.kt
+++ app/src/main/java/com/orion/templete/ui/components/CostomBar.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun DisplayBar(leftText: String, middleText: String, rightText: String) {
+fun DisplayBar(leftText: String = "null", middleText: String = "null", rightText: String = "null") {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(16.dp)
